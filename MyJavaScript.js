c5e7809fae9a320a1d29e1b91cb5ae6564ab5a6f
Azizi <munaemazizi@gmail.com>
2022-08-28 00:21:08 -0700

    add file

---
+++ MyJavaScript.js
@@ -0,0 +1,27 @@
+
+
+
+// var x=10;
+// var y=20;
+// var z=x+y;
+
+// document.write(z);
+
+
+// var name = "Ahmad";
+
+// {
+//     var name = "Mohammad";
+// }
+
+// alert(name)
+
+
+
+// let name = "Ahmad";
+// {
+//     let name = "Mohammad";
+// }
+
+// alert(name)
+
